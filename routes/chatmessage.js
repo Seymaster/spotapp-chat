0dbfb26d98085327b3b63c747fd474ca93fc3d5e
Seymaster <alugbinoluwaseyi1@gmail.com>
2020-08-13 10:26:14 +0100

    last commit

--- routes/chatmessage.js
+++ routes/chatmessage.js
@@ -6,6 +6,10 @@ const router  = express.Router();
 // POST /send chat message
 router.get("/message", (req,res,next)=>{
     console.log("Socket is opened")
+    res.status(200).send({
+        status: 200,
+        message: "Socket is opened"
+    })
 })
 
 
